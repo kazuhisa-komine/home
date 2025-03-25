0b3771c03bf63783ecbd680e00fcc5754c118c8e
kazuhisa-komine <k.komine@u-p-life.com>
2025-03-25 17:51:34 +0900

    Add files via upload

---
+++ js_safe_fixed_v2.js
@@ -0,0 +1,15 @@
+
+$(function () {
+  if ($(".top").length > 0) {
+    if ($('.top').length > 0) {
+  var top = $('.top').offset().top;
+}
+  }
+
+  // 以下は既存のJS処理（仮定） - ここに必要な追加処理を入れる
+  // 例：
+  // $('.some-element').on('click', function () {
+  //   ...
+  // });
+
+});
