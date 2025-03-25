0f8cc44270dbc9420a6a3540c1599ffea980dd5d
kazuhisa-komine <k.komine@u-p-life.com>
2025-03-25 18:02:09 +0900

    Update js_safe_fixed_v2.js

--- js_safe_fixed_v2.js
+++ js_safe_fixed_v2.js
@@ -1,7 +1,7 @@
 $(function () {
-  if ($('.top').length > 0) {
-    var top = $('.top').offset().top;
-    // console.log(top); ← 必要なら位置を確認できるよ
+  if ($('#top').length > 0) {
+    var top = $('#top').offset().top;
+    // console.log(top); ← 確認したいならここで
   }
 
   // 以下は既存のJS処理（仮定） - ここに必要な追加処理を入れる
