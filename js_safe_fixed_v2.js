d512857bcf3f45aca2cb31afba3c0400e671eb6c
kazuhisa-komine <k.komine@u-p-life.com>
2025-04-16 12:57:31 +0900

    Delete js_safe_fixed_v2.js

--- js_safe_fixed_v2.js
+++
@@ -1,9 +0,0 @@
-$(window).on('load', function () {
-  const $top = $('#top');
-  if ($top.length > 0) {
-    const topOffset = $top.offset().top;
-    console.log(topOffset); // ← 位置を確認したい時だけ表示
-  }
-
-  // 他の処理があればここに書いてね
-});
